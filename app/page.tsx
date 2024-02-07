0c3d576e2a034265e06a89c8eafc353dad6b9a17
r2hu1 <newasram86@gmail.com>
2024-02-07 15:13:44 +0530

    []

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Dialog from "@/components/Dialog";
 import Hero from "@/components/site/Hero";
 
 export default function Home() {
   return (
     <div className="py-20 grid gap-4">
       <Hero/>
+      <Dialog/>
     </div>
   );
 }
---
+++ components/Dialog.tsx
@@ -0,0 +1,9 @@
+export default function Dialog(){
+    return(
+        <div className="h-full w-full fixed top-0 left-0 right-0 flex items-center justify-center">
+            <div>
+                
+            </div>
+        </div>
+    )
+};
