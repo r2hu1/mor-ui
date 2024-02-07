8532e5f8bb3d45823a4467585ed23ad681fd7481
r2hu1 <newasram86@gmail.com>
2024-02-07 21:41:05 +0530

    some fixes

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 ## Mor-Ui
 
-A !ui Library built with Nextjs, Tailwindcss.
-
-## Avilable Blocks
-
-Button, Input, Tooltip etc, looking forward to add more..
+A reuseable components !library built using [React](https://reactjs.org) and [Tailwind CSS](https://tailwindcss.com) with [Next.js](https://nextjs.org) and [Typescript](https://www.typescriptlang.org)
 
 ## Preview
 
-![preview](public/preview.png)
+![preview](public/preview-dark.png)
+
+![preview](public/preview-light.png)
+
+## Contribution
+
+- Fork it
+- Pull request
+- Create your feature branch (git checkout -b branch-name)
+- Commit your changes (git commit -m "Add some description")
+- Push to the branch (git push origin branch-name)
+- Open a PR
+
+## License
+
+MIT License
--- components/site/Hero.tsx
+++ components/site/Hero.tsx
@@ -14,16 +14,22 @@ export default function Hero() {
                     <Button className="mt-5" variant="secondary">Docs</Button>
                 </Tooltip>
             </div>
-            <div className="relative h-[300px] max-w-2xl mx-auto flex border border-secondary items-center justify-center gap-2 p-4 w-full bg-secondary/70 mt-16 rounded-lg">
-                <div className="flex gap-2 absolute top-2 right-2">
-                    <Tooltip content="Live preview">
-                        <Button className="text-xs h-6">preview</Button>
-                    </Tooltip>
-                    <Tooltip content="Source code">
-                        <Button variant="secondary" className="text-xs h-6" asLink href="https://github.com/r2hu1/mor-ui/blob/master/components/Button.tsx">code</Button>
-                    </Tooltip>
+            <div className="relative h-[300px] max-w-2xl mx-auto flex flex-wrap border border-secondary items-center justify-center gap-2 p-4 w-full bg-secondary/60 mt-16 rounded-lg">
+                <div className="flex items-center justify-between w-full absolute top-0 right-0 p-2 px-3 border-b border-secondary">
+                    <span className="text-xs">Button.jsx</span>
+                    <div className="flex gap-2 items-center">
+                        <Tooltip content="Live preview">
+                            <Button className="text-xs h-6">preview</Button>
+                        </Tooltip>
+                        <Tooltip content="Source code">
+                            <Button variant="secondary" className="text-xs h-6" asLink href="https://github.com/r2hu1/mor-ui/blob/master/components/Button.tsx">code</Button>
+                        </Tooltip>
+                    </div>
                 </div>
-                <Button>Button</Button>
+                <Button>Primary</Button>
+                <Button variant="secondary">Secondary</Button>
+                <Button variant="outline">Outline</Button>
+                <Button variant="ghost">Ghost</Button>
             </div>
         </div>
     )
