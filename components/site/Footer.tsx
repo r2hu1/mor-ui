555dc3a35c36605a3d34ecabd9fe1dd3eee35b1c
r2hu1 <newasram86@gmail.com>
2024-02-09 22:27:06 +0530

    fixed footer

--- components/site/Footer.tsx
+++ components/site/Footer.tsx
@@ -1,9 +1,11 @@
+import { Minus } from "lucide-react";
+
 export default function Footer() {
     return(
-        <footer className="py-10 grid gap-1">
-            <div className="flex items-center justify-center gap-2">
+        <footer className="py-10 grid gap-1 opacity-85">
+            <div className="flex items-center justify-center gap-1">
                 <span className="text-sm">Mor-UI</span>
-                <span className="text-xs">|</span>
+                <Minus className="w-[1rem] h-[1rem] -rotate-90" />
                 <span className="text-sm">{new Date().getFullYear()}</span>
             </div>
             <p className="text-center text-xs">Built by <a className="opacity-70" href="https://github.com/r2hu1">r2hu1</a></p>
